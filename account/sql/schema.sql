18ff19243d2162a28bd30f5744ab863581c91a01
shenlan <manbuzhe2009@qq.com>
2025-10-10 13:54:15 +0800

    Fix schema initialization permissions (#464)

--- account/sql/schema.sql
+++ account/sql/schema.sql
@@ -5,7 +5,7 @@
 -- =========================================
 
 DROP SCHEMA IF EXISTS public CASCADE;
-CREATE SCHEMA public AUTHORIZATION postgres;
+CREATE SCHEMA public AUTHORIZATION CURRENT_USER;
 
 -- =========================================
 -- Extensions
